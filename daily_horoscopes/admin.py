ce0fe115ecc00b23be46010e500614e9218c9ba0
ivan-shumiln <shumilin.i.v@gmail.com>
2022-06-20 12:05:00 +0300

    вывод Base в админ-панель

--- daily_horoscopes/admin.py
+++ daily_horoscopes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Menu
+from .models import Menu, Base
 from rest_framework.authtoken.admin import TokenAdmin
 
 admin.site.register(Menu)
+admin.site.register(Base)
 TokenAdmin.raw_id_fields = ['user']
