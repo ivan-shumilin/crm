ede00426fc27f5e342ef2b9cbb2f6de16dddd870
ivan-shumiln <shumilin.i.v@gmail.com>
2022-06-22 18:33:30 +0300

    подключил базу к интерфейсу

--- daily_horoscopes/urls.py
+++ daily_horoscopes/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     # path('api/forecast/', views.GetForecastInfoView.as_view()),
     path('', views.index, name='index'),
+    # path('form/', views.form, name='form'),
     path('register/', views.register, name='register'),
     path('accounts/login/', views.user_login, name='login'),
 ]
