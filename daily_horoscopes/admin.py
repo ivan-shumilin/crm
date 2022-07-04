4acfabd1211b4d525fbbfa1acee95c57b413e020
ivan-shumiln <shumilin.i.v@gmail.com>
2022-07-04 12:14:55 +0300

    вывод cooking_method в admin

--- daily_horoscopes/admin.py
+++ daily_horoscopes/admin.py
@@ -5,7 +5,7 @@ from .models import Base, Product, Timetable
 admin.site.register(Base)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ovd', 'shd', 'bd', 'vbd', 'nbd', 'nkd', 'vkd', 'category')
+    list_display = ('name','cooking_method', 'ovd', 'shd', 'bd', 'vbd', 'nbd', 'nkd', 'vkd', 'category')
     fields = ('name', 'ovd', 'shd', 'bd', 'vbd', 'nbd', 'nkd', 'vkd', 'category')
     list_filter = ('category', 'ovd', 'shd', 'bd', 'vbd', 'nbd', 'nkd', 'vkd')
     # list_filter = ('status', 'due_back')
