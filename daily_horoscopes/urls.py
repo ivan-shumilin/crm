8e853c70c89a8c3c4e71a257446fcaf6a1aa5aa6
ivan-shumiln <shumilin.i.v@gmail.com>
2022-07-08 13:19:56 +0300

    Добавил вывод всех блюд по категориям

--- daily_horoscopes/urls.py
+++ daily_horoscopes/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('catalog/salad/<page>', views.catalog_salad, name='salad'),
+    path('catalog/soup/<page>', views.catalog_soup, name='soup'),
+    path('catalog/main_dishes/<page>', views.catalog_main_dishes, name='main_dishes'),
+    path('catalog/side_dishes/<page>', views.catalog_side_dishes, name='side_dishes'),
     path('backup/', views.backup, name='backup'),
     path('accounts/login/', views.user_login, name='login'),
 ]
